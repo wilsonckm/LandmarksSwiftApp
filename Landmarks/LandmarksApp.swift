f024c9dd62c59d54f798b4f6f86d007e8046a16a
wilsonckm <wilsonckmchan@gmail.com>
2023-09-14 13:23:08 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Wilson Chan on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
